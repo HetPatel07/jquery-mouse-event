fbfa0dd3a1d41d9bdb42cf650ee6bf16dee4b9ef
hetpatel07 <hets7878401023@gmail.com>
2019-05-30 17:25:24 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -9,19 +9,40 @@ $(document).ready(function () {
 
     })
     $(document).click(function () {
+        var hue = Math.floor((Math.random() * 360) + 1);
+        var sat = Math.floor((Math.random() * 100) + 1) + "%";
+        var l = Math.floor((Math.random() * 100) + 1) + "%";
         var div1 = document.createElement('div');
         var main = document.getElementsByClassName('main')[0];
         div1.setAttribute('id', 'box');
         div1.setAttribute('class', 'box');
         main.appendChild(div1);
+        var color = 'hsl(' + hue + ',' +
+            sat + ',' + l + ')';
+        console.log(color);
+                div1.style.background = color;
 
-        if (i == 0) {
-            div1.style.background = "blue";
-            i = 1;
-        } else {
-            div1.style.background = "red";
-            i = 0;
-        }
+        //        if (x == 1) {
+        //            div1.style.background = "blue";
+        //        } else if (x == 2) {
+        //            div1.style.background = "orange";
+        //        } else if (x == 3) {
+        //            div1.style.background = "pink";
+        //        } else if (x == 4) {
+        //            div1.style.background = "purple";
+        //        } else if (x == 5) {
+        //            div1.style.background = "pink";
+        //        } else if (x == 6) {
+        //            div1.style.background = "black";
+        //        } else if (x == 7) {
+        //            div1.style.background = "magenta";
+        //        } else if (x == 8) {
+        //            div1.style.background = "cyan";
+        //        } else if (x == 9) {
+        //            div1.style.background = "sea green";
+        //        } else if (x == 10) {
+        //            div1.style.background = "hot pink";
+        //        }
         console.log(div1.previousElementSibling);
         div1.previousElementSibling.removeAttribute('id');
     })
